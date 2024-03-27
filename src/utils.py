3788b06f6e0b60fc36ea2ac35bd834c8b610fe63
Fabian <radomirfabian00@gmail.com>
2024-03-27 22:34:56 +0200

    removing unnecessary code

--- src/utils.py
+++ src/utils.py
@@ -39,35 +39,3 @@ def catch_errors(function):
             warnings.warn(f"Error in function {(function.__name__).upper()}: {e}")
 
     return handler
-
-@catch_errors
-def remove_prefix_from_addresses(addresses: list) -> list:
-    """
-    Function will look for unnecessary words within the address and remove them, keeping strictly the address itself
-    :param addresses: list of addresses
-    :type addresses: list
-    :return: returns the same list with cleaned elements
-    """
-    def remove_prefix(address: list) -> list:
-        pattern = r"[\w\s]*:\s*(.*)"  # Match any prefix followed by zero or more whitespace characters
-        address = str(address[0])  # Convert passed argument from a list element to a string
-        match = re.search(pattern, address)  # Use regex to find the matched part of the address
-        if match:
-            return match.group(1).strip()  # Extract the matched part (group 1) and remove leading/trailing whitespace
-        else:
-            return address  # Return the original address if no match is found
-    def remove_commas(address: list) -> list:
-        no_comma_address = re.sub(",","",address)
-        return no_comma_address
-    def remove_non_address(address: list) -> list:
-        pattern = re.compile(r"[0-9]+ ([A-Za-z]+( [A-Za-z]+)+) ([A-Za-z0-9]+( [A-Za-z0-9]+)+)",re.IGNORECASE)
-        address = str(address[0])  # Convert passed argument from a list element to a string
-        match = re.search(pattern, address)  # Use regex to find the matched part of the address
-        if match:
-            return address  # Extract the matched part (group 1) and remove leading/trailing whitespace
-        else:
-            pass
-    cleansed_addresses = [remove_prefix(address) for address in addresses]
-    cleansed_addresses = [remove_commas(address) for address in cleansed_addresses]
-    cleansed_addresses = [remove_non_address(address) for address in cleansed_addresses]
-    return cleansed_addresses
